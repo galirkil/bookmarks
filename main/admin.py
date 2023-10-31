d6d10bd6512c189c122056eb5cc68ac64c9cf2b7
galirkil <galirkil@gmail.com>
2023-10-31 14:37:24 +0300

    Add bookmark model

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import Bookmark
+
+
+@admin.register(Bookmark)
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ('url', 'user', )
+    list_filter = ('user', )
--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Bookmark(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=256)
+    url = models.URLField()
+    description = models.TextField()
+    favicon = models.ImageField(upload_to='url_icons', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'закладка'
+        verbose_name_plural = 'закладки'
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'url'], name='unique_url')
+        ]
