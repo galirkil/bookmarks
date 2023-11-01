e0899b1f4f84d713e1b4b4beba6eb82809a32838
galirkil <galirkil@gmail.com>
2023-11-02 00:22:51 +0300

    Refactor add bookmark view

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from main.views import BookmarkCreateView, BookmarksListView
+from main.views import BookmarksListView, add_bookmark
 
 app_name = 'main'
 
 urlpatterns = [
     path('my-bookmarks/', BookmarksListView.as_view(), name='my-bookmarks'),
-    path('add-bookmark/', BookmarkCreateView.as_view(), name='add-bookmark')
+    path('add-bookmark/', add_bookmark, name='add-bookmark')
 ]
--- main/views.py
+++ main/views.py
@@ -1,11 +1,13 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.messages.views import SuccessMessageMixin
-from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
 from django.views.generic import ListView
 from django.views.generic.base import TemplateView
-from django.views.generic.edit import CreateView
 
 from common.views import TitleMixin
+from main.forms import BookmarkForm
 from main.models import Bookmark
 from main.tasks import get_bookmark_info
 
@@ -23,16 +25,20 @@ class BookmarksListView(LoginRequiredMixin, TitleMixin, ListView):
         return Bookmark.objects.filter(user=self.request.user)
 
 
-class BookmarkCreateView(LoginRequiredMixin, TitleMixin, SuccessMessageMixin, CreateView):
-    template_name = 'main/add-bookmark.html'
-    title = 'Bookmarks - Добавить закладку'
-    model = Bookmark
-    fields = ('url', )
-    success_url = reverse_lazy('main:add-bookmark')
-    success_message = 'Вы успешно добавили новую закладку!'
+def add_bookmark(request):
+    if request.method == 'POST':
+        bookmark = Bookmark(user=request.user)
+        form = BookmarkForm(data=request.POST, instance=bookmark)
+        if form.is_valid():
+            bookmark.save()
+            messages.success(request, 'Вы успешно добавили закладку!')
+            get_bookmark_info.delay(request.POST['url'], request.user.id)
+            return HttpResponseRedirect(reverse('main:add-bookmark'))
+    else:
+        form = BookmarkForm()
+    context = {
+        'form': form,
+        'title': 'Bookmarks - Добавить закладку',
+    }
+    return render(request, 'main/add-bookmark.html', context)
 
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        response = super().form_valid(form)
-        get_bookmark_info.delay(self.request.POST['url'], self.request.user.id)
-        return response
