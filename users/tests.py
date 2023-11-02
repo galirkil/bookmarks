60d58ee67c097ababec35b991a44c1f84273d27a
galirkil <galirkil@gmail.com>
2023-11-02 03:20:10 +0300

    Add some unittests for users app

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+from http import HTTPStatus
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+from users.models import User
+
+
+class UserRegistrationViewTestCase(TestCase):
+
+    def setUp(self):
+        self.path = reverse('users:register')
+        self.data = {
+            'first_name': 'test', 'last_name': 'case',
+            'username': 'test_case', 'email': 'test_case@gmail.com',
+            'password1': 'osdfj-0203ljsd', 'password2': 'osdfj-0203ljsd',
+        }
+
+    def test_user_registration_get(self):
+        response = self.client.get(self.path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Bookmarks - Регистрация')
+        self.assertTemplateUsed(response, 'users/register.html')
+
+    def test_user_registration_post_success(self):
+
+        username = self.data['username']
+        self.assertFalse(User.objects.filter(username=username).exists())
+        response = self.client.post(self.path, self.data)
+
+        # check if user is created
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('users:login'))
+        self.assertTrue(User.objects.filter(username=username).exists())
+
+    def test_user_registration_post_error(self):
+        User.objects.create(username=self.data['username'])
+        response = self.client.post(self.path, self.data)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertContains(response, 'A user with that username already exists.', html=True)
