799d78f95be654e06c8f4fed62420daece60cb54
galirkil <galirkil@gmail.com>
2023-11-02 10:10:51 +0300

    Add some test for main

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from http import HTTPStatus
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+
+class MainIndexViewTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.path = reverse('index')
+
+    def test_main_index_get(self):
+        response = self.client.get(self.path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Bookmarks - Главная')
+        self.assertTemplateUsed(response, 'main/index.html')
+
+
+class MainBookmarksListViewTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.path = reverse('main:my-bookmarks')
+
+    def test_main_bookmarks_list_get(self):
+        response = self.client.get(self.path)
+
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertTemplateUsed(response, 'main/bookmarks.html')
